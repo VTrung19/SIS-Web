bb64ab4ae132d0acaa31af65ae2088bbdd5c987f
pquyenn <21521368@gm.uit.edu.vn>
2024-11-22 01:22:44 +0700

    add docker files

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# SIS-Web
+Install docker and docker compose and run: 
+
+```
+docker compose up -d
+```
+
+Access to `localhost:9000`
---
+++ src/Project/php/Controller/connect.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "db";
+$username = "testuser";
+$password = "testpassword";
+$dbname = "shoe_shop_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
